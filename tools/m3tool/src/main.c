04ff685d55c24b8659ea56b14c4ae984917051a7
lolguy91 <retek1544@gmail.com>
2025-11-10 18:53:14 +0100

    dump music order

--- tools/m3tool/src/main.c
+++ tools/m3tool/src/main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <endian.h>
@@ -65,6 +66,87 @@ typedef struct {
     uint32_t smell_weakness;
 } enemy_t;
 
+#define MUSIC_ORDER_OFF   	0xECD40
+#define MUSIC_ORDER_ENTRIES 0x100
+
+typedef struct {
+    uint16_t song_ptr;
+    uint16_t music_player_num;
+    uint32_t required_chapter;
+    uint32_t duration;
+} music_t;
+
+void print_enemy_order(FILE* f){
+    fseek(f,ENEMY_ORDER_OFF,SEEK_SET);
+    uint16_t enemy_order[ENEMY_ORDER_ENTRIES];
+
+    fread(enemy_order,2,ENEMY_ORDER_ENTRIES,f);
+
+    printf("Battle memory enemy order:\n");
+    for (size_t i = 0; i < ENEMY_ORDER_ENTRIES; i++){
+        uint16_t entry = le16toh(enemy_order[i]);
+
+        if(entry == 0xffff) //end signal
+            break;
+
+        printf("  %zu: 0x%x\n",i,entry);
+
+    }
+}
+
+void print_enemy_data(FILE* f){
+    fseek(f,ENEMY_DATA_OFF,SEEK_SET);
+    enemy_t enemies[ENEMY_DATA_ENTRIES];
+
+    fread(enemies,sizeof(enemy_t),ENEMY_DATA_ENTRIES,f);
+
+    printf("Enemy data:\n");
+    for (size_t i = 0; i < 0xBE /*Let's try to not show garbage data*/; i++){
+        enemy_t entry = enemies[i];
+
+        printf("==%2zx===\n  ID:            0x%x\n",i,entry.enemy_id);
+        printf("  Background:               0x%x\n",entry.background);
+        printf("  Transition music:         0x%x\n",entry.transition_music);
+        printf("  Battle music:             0x%x\n",entry.battle_music);
+        printf("  Win music:                0x%x\n",entry.win_music);
+        printf("  Level:                    0x%x\n",entry.level);
+        printf("  HP:                       0x%x\n",entry.hp);
+        printf("  PP:                       0x%x\n",entry.pp);
+        printf("  Offense:                  0x%x\n",entry.offense);
+        printf("  Defense:                  0x%x\n",entry.defense);
+        printf("  IQ:                       0x%x\n",entry.iq);
+        printf("  Speed:                    0x%x\n",entry.speed);
+        printf("  Kindness:                 0x%x\n",entry.kindness);
+        printf("  Offense(suprise attack):  0x%x\n",entry.suprise_offense);
+        printf("  Defense(suprise attack):  0x%x\n",entry.suprise_defense);
+        printf("  IQ(suprise attack):       0x%x\n",entry.suprise_iq);
+        printf("  Speed(suprise attack):    0x%x\n",entry.suprise_speed);
+        printf("  Kindness(suprise attack): 0x%x\n",entry.suprise_kindness);
+        printf("  Dropped experience:       0x%x\n",entry.drop_exp);
+        printf("  Dropped money:            0x%x\n",entry.drop_money);
+        printf("  Defeat animation:         0x%x\n",entry.defeat_animation);
+        printf("  Defeat anim(last enemy):  0x%x\n",entry.last_defeat_anim);
+    }
+}
+
+void print_music_order(FILE* f){
+    fseek(f,MUSIC_ORDER_OFF,SEEK_SET);
+    music_t musics[MUSIC_ORDER_ENTRIES];
+
+    fread(musics,sizeof(music_t),MUSIC_ORDER_ENTRIES,f);
+
+    for (size_t i = 0; i < MUSIC_ORDER_ENTRIES; i++){
+        music_t entry = musics[i];
+
+        printf("==%2zx===\n",i);
+        printf("  Song ptr:                 0x%x\n",entry.song_ptr);
+        printf("  Music player num:         %u\n",entry.music_player_num);
+        printf("  Required chapter:         0x%x\n",entry.required_chapter);
+        printf("  Duration(unit?):        0x%x\n",entry.duration);
+    }
+
+}
+
 int main(int argc, char** argv){
     printf("=====M3TOOL vDevelopment====\n");
     if(argc < 2){
@@ -87,57 +169,10 @@ int main(int argc, char** argv){
     uint32_t size = ftell(f);
     printf("ROM size: %uM\n",size / 1000000);
 
-    fseek(f,ENEMY_ORDER_OFF,SEEK_SET);
-    uint16_t enemy_order[ENEMY_ORDER_ENTRIES];
+    print_enemy_order(f);
+    //print_enemy_data(f);
+    print_music_order(f);
 
-    fread(enemy_order,2,ENEMY_ORDER_ENTRIES,f);
-
-    printf("Battle memory enemy order:\n");
-    for (size_t i = 0; i < ENEMY_ORDER_ENTRIES; i++){
-        uint16_t entry = le16toh(enemy_order[i]);
-
-        if(entry == 0xffff) //end signal
-            break;
-
-        printf("  %zu: 0x%x\n",i,entry);
-
-    }
-
-    fseek(f,ENEMY_DATA_OFF,SEEK_SET);
-    enemy_t enemies[ENEMY_DATA_ENTRIES];
-
-    fread(enemies,sizeof(enemy_t),ENEMY_DATA_ENTRIES,f);
-
-    
-    printf("Enemy data:\n");
-    for (size_t i = 0; i < 0xBE /*Let's try to not show garbage data*/; i++){
-        enemy_t entry = enemies[i];
-
-        printf("==%2zx===\n  ID: 0x%x\n",i,entry.enemy_id);
-        printf("  Background:               0x%x\n",entry.background);
-        printf("  Transition music:         0x%x\n",entry.transition_music);
-        printf("  Battle music:             0x%x\n",entry.battle_music);
-        printf("  Win music:                0x%x\n",entry.win_music);
-        printf("  Level:                    0x%x\n",entry.level);
-        printf("  HP:                       0x%x\n",entry.hp);
-        printf("  PP:                       0x%x\n",entry.pp);
-        printf("  Offense:                  0x%x\n",entry.offense);
-        printf("  Defense:                  0x%x\n",entry.defense);
-        printf("  IQ:                       0x%x\n",entry.iq);
-        printf("  Speed:                    0x%x\n",entry.speed);
-        printf("  Kindness:                 0x%x\n",entry.kindness);
-        printf("  Offense(suprise attack):  0x%x\n",entry.suprise_offense);
-        printf("  Defense(suprise attack):  0x%x\n",entry.suprise_defense);
-        printf("  IQ(suprise attack):       0x%x\n",entry.suprise_iq);
-        printf("  Speed(suprise attack):    0x%x\n",entry.suprise_speed);
-        printf("  Kindness(suprise attack): 0x%x\n",entry.suprise_kindness);
-        printf("  Dropped experience:       0x%x\n",entry.drop_exp);
-        printf("  Dropped money:            0x%x\n",entry.drop_money);
-        printf("  Defeat animation:         0x%x\n",entry.defeat_animation);
-        printf("  Defeat anim(last enemy):  0x%x\n",entry.last_defeat_anim);
-
-
-    }
 
     fclose(f);
 }
